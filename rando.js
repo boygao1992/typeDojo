3016dc10519fb9b91cd0b58e7fc1f041c2148aa6
boygao1992 <wg2313@columbia.edu>
2018-07-25 01:30:25 -0400

    less ad-hoc rando

--- rando.js
+++ rando.js
@@ -4,22 +4,68 @@ const {
   //ADT
   Pair,
   State,
+  // Maybe
+  option,
+  prop,
+  // State
+  State: {
+    modify,
+    get,
+  },
   // Helper
   assign,
+  chain,
+  map,
+  // Combinator
+  compose,
+  constant,
 } = require( 'crocks' )
 
-const rando = () => State( s => {
-  const seed = ( 1103515245 * s.seed + 12345 ) & 0x7fffffff
-  const value = ( seed >> 16 ) / 0x7fff
+// const rando = () => State( s => {
+//   const seed = ( 1103515245 * s.seed + 12345 ) & 0x7fffffff
+//   const value = ( seed >> 16 ) / 0x7fff
 
-  return Pair( value, assign( { seed }, s ) )
-} )
+//   return Pair( value, assign( { seed }, s ) )
+// } )
 
-log(
-  rando()
-  .chain( rando )
-  .chain( rando )
-  .runWith( { seed: 23 } )
-)
+// log(
+//   rando()
+//   .chain( rando )
+//   .chain( rando )
+//   .runWith( { seed: 23 } )
+// )
 
 // Could also be modeled as a Writer, where the Monoid on the right is Last Monoid
+
+const pluckSeed =
+  def => compose(
+    option( def ),
+    prop( 'seed' )
+  )
+
+const rando = x => {
+  const seed = ( 1103515245 * x + 12345 ) & 0x7fffffff
+  const value = ( seed >>> 16 ) / 0x7fff
+
+  return compose(
+    map( constant( value ) ),
+    modify,
+    assign
+  )( { seed } )
+}
+
+const pullRandom =
+  compose(
+    chain( rando ),
+    get,
+    pluckSeed
+  )
+
+const seed = 73
+
+log(
+  State.of( seed )
+  .chain( pullRandom )
+  .chain( pullRandom )
+  .runWith( {} )
+)
