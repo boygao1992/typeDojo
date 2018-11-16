f530d4cce207c0648e3c2286eab4807c02597364
boygao1992 <wg2313@columbia.edu>
2018-11-16 05:08:11 -0500

    dojo-web-page: timer working; TODO: fix initial time, add pause/resume by space key

--- dojo-web-page/src/Components/ViewComponent.purs
+++ dojo-web-page/src/Components/ViewComponent.purs
@@ -14,23 +14,26 @@ import Data.FoldableWithIndex (foldlWithIndex)
 import Data.Maybe (Maybe(..))
 import Data.String (length) as String
 import Data.String.CodeUnits (dropRight, toCharArray) as String
-import Data.Time.Duration (Milliseconds)
+import Data.Time.Duration (Milliseconds(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Now (now) as Time
+import Effect.Timer (setInterval, clearInterval) as Timer
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.CSS as HC
 import Halogen.Query.EventSource (eventSource') as HES
 import HalogenUtils (classList_)
+import Math as Math
 import ShowChar (showChar)
 import Web.Event.Event (preventDefault) as WE
 import Web.Event.EventTarget (addEventListener, eventListener, removeEventListener) as WE
 import Web.HTML (HTMLDocument)
 import Web.HTML (window) as DOM
 import Web.HTML.HTMLDocument as HTMLDocument
-import Web.HTML.Window (document) as DOM
+import Web.HTML.Window (Window)
+import Web.HTML.Window (document, requestAnimationFrame) as DOM
 import Web.UIEvent.KeyboardEvent (KeyboardEvent, fromEvent, key, toEvent) as KE
 import Web.UIEvent.KeyboardEvent.EventTypes (keydown) as KE
 
@@ -41,6 +44,13 @@ addOnKeyDownEventListener document fn = do
   listener <- WE.eventListener (traverse_ fn <<< KE.fromEvent)
   WE.addEventListener KE.keydown listener false target
   pure $ WE.removeEventListener KE.keydown listener false target
+
+addTimer :: Window -> (Milliseconds -> Effect Unit) -> Effect (Effect Unit)
+addTimer window fn = do
+  id <- Timer.setInterval 50 do
+    _ <- DOM.requestAnimationFrame (fn =<< Time.unInstant <$> Time.now) window
+    pure unit
+  pure $ Timer.clearInterval id
 
 -- | Predicates
 isPrint :: String -> Boolean
@@ -65,12 +75,29 @@ isRight = (_ == "ArrowRight")
 
 type TimeStamp = Milliseconds
 
+viewTimer :: Milliseconds -> String
+viewTimer (Milliseconds ms) =
+  let
+    minuteLength = 60000.0
+    secondLength = 1000.0
+    minutes = Math.floor (ms / minuteLength)
+    seconds = Math.floor ((ms - (minutes * minuteLength)) / secondLength)
+    milliseconds = ms - (minutes * minuteLength + seconds * secondLength)
+  in
+    show minutes <> " : " <> show seconds <> " : " <> show milliseconds
+
 type State =
   { history :: Array KeyStroke
   , input :: String
   , dojo :: String
   , cursor :: Int
+  , timer :: Milliseconds
   }
+
+data Status
+  = Stoped
+  | Playing
+  | Paused
 
 type KeyStroke =
   { key :: String
@@ -98,11 +125,13 @@ initialState =
   , input : ""
   , dojo : "\".zww?]M#=uh9F:%^qqE(W(=$D*x\"Kw7'@h+\\ELI{v?N\\$ySJ<i%\"jE.W@}u7An5:%q%{)_[_OEu#b(BxM=A$\\;25tR):dvM,:4r7&/D.X#Du_?1~F+I6Mvy(u>)]oir,YM)K5LoMQ~]#\\(kl8IO+Iv87c==;bx}Dwqa}YM4yC5/2*?%L,;_mix>ca?9%'BT4O<b!{irs]zH%mgE]wb#CM9DK{!^OH;sh&$/.E<I}].[ZDoiO6V9*p\\U@[N9\\G@.=ccW+>uc7<D<qE*rMV^^*_JZdLdP!EFTP)fnyn2gP1C\""
   , cursor : 0
+  , timer : mempty
   }
 
 data Query next
   = Init next
   | OnKeyDown KE.KeyboardEvent (H.SubscribeStatus -> next)
+  | Tick Milliseconds (H.SubscribeStatus -> next)
 
 type Input = Unit
 
@@ -126,9 +155,11 @@ charBlock isCursor { key, status } =
 -- | Component
 
 render :: State -> H.ComponentHTML Query
-render { input , dojo , cursor } =
+render { input , dojo , cursor, timer } =
   HH.div_
-  [ HH.p_ $
+  [ HH.p_
+    [ HH.text $ viewTimer timer]
+  , HH.p_ $
     foldlWithIndex (\idx acc item -> acc `A.snoc` (charBlock (cursor == idx) item)) [] processed
   , HH.p_
     [ HH.text $ "input: " <> show input]
@@ -155,8 +186,10 @@ render { input , dojo , cursor } =
 
 eval :: Query ~> H.ComponentDSL State Query Output IO
 eval (Init next) = next <$ do
-  document <- H.liftEffect $ DOM.document =<< DOM.window
+  window <- H.liftEffect DOM.window
+  document <- H.liftEffect $ DOM.document window
   H.subscribe $ HES.eventSource' (addOnKeyDownEventListener document) (Just <<< H.request <<< OnKeyDown)
+  H.subscribe $ HES.eventSource' (addTimer window) (Just <<< H.request <<< Tick)
 
   { body } <- H.liftAff $ AX.get AX.string url
   let dojo = either (const "") identity body
@@ -190,6 +223,9 @@ eval (OnKeyDown ev reply) = do
     when (state.cursor < String.length state.dojo - 1) do
       H.modify_ _ { cursor = state.cursor + 1 }
 
+  pure $ reply H.Listening
+eval (Tick ms reply) = do
+  H.modify_ _ { timer = ms }
   pure $ reply H.Listening
 
 component :: H.Component HH.HTML Query Input Output IO
